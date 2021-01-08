031551ce1d32cc9ed007f8572b43511d93729c55
Tyson Heesacker <tysonheesacker86@gmail.com>
2021-01-08 10:33:08 -0800

    updating table

--- static/js/app.js
+++ static/js/app.js
@@ -28,4 +28,29 @@ function runEnter(){
 
     var filteredSights = tableData.filter(alien => alien.datetime === inputValue);
     console.log(filteredSights)
+
+    // Create variables for table data
+    // var alienTime = filteredSights.map(alien => alien.datetime);
+    // var alienCity = filteredSights.map(alien => alien.city);
+    // var alienState = filteredSights.map(alien => alien.state);
+    // var alienCountry = filteredSights.map(alien => alien.country);
+    // var alienShape = filteredSights.map(alien => alien.shape);
+    // var alienDuration = filteredSights.map(alien => alien.duration);
+    // var alienComments = filteredSights.map(alien => alien.comments);
+    data.forEach(function(filteredSights){
+    console.log(filteredSights);
+    var dataRow = tbody.append("tr");
+
+    Object.entries(filteredSights).forEach(function([key, value]){
+        console.log(key, value);
+        var dataCell = row.append("td");
+        dataCell.text(value);
+        });
+    });
+    
+
+    // //locate and append table in HTML
+    // var alTable = d3.select("#ufo-table");
+    // alTable.append("tr").text(alienTime);
+    // alTable.append("tr").text(alienCity);
 };
