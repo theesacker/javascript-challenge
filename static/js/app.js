790d9c2f06bb412f59ef74855053dde8fbe44eb1
Tyson Heesacker <tysonheesacker86@gmail.com>
2021-01-07 19:47:59 -0800

    creating input element

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Create variable to select form
+var form = d3.select("#datetime");
+
+// Create variable to select button
+var button = d3.select("#filter-btn");
+
+// Create event handlers
+
+form.on("submit", runEnter);
+button.on("click", runEnter);
+
+// To complete the event handler function for the form
+function runEnter(){
+
+// To prevent the page from refreshing:
+    d3.event.preventDefault();
+
+    // Select Raw HTML node:
+    var inputElement = d3.select("#datetime");
+
+    // Get value of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputElement);
+    console.log(inputValue);
+
+};
